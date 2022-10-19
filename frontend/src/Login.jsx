7386e3a5340f90c613735b2a44e913590e7b5b0a
Felipe Godoy <felipe-est@hotmail.com>
2022-10-19 18:23:30 -0300

    Update Login.jsx
    Form included, changes to styles.

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,5 +1,8 @@
 
 import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import './styles/login.css'
 
 export default function Login () {
   return (
@@ -10,9 +13,17 @@ export default function Login () {
           Iniciar sessão
         </span>
         <div className='content__form'>
-          <form>
-
-          </form>
+          <Form>
+            <Form.Group className="mb-3 text-start" controlId="formEmail">
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control type="email" placeholder="Insira seu e-mail"></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3 text-start" controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="* * * * * * * *"></Form.Control>
+            </Form.Group>
+            <Button className="form__button">Entrar</Button>
+          </Form>
         </div>
       </div>
     </div>
