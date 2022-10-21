1f75f18415492d428744c25140811d182a3551d2
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-10-21 08:56:01 -0300

    up

--- frontend/src/components/Searchbar/Searchbar.jsx
+++ frontend/src/components/Searchbar/Searchbar.jsx
@@ -1,8 +1,9 @@
 import Button from "react-bootstrap/Button";
-
+import React, { useRef } from "react";
 import "./Searchbar.css";
 
 export default function Searchbar() {
+  const ref = useRef();
   return (
     <>
       <div className="d-flex w-75 flex-column flex-lg-row justify-content-center">
@@ -14,14 +15,32 @@ export default function Searchbar() {
             placeholder="Onde Vamos?"
           />
         </div>
+        
         <div className="form-group has-search m-1">
           <span className="bi bi-calendar4-week form-control-feedback"></span>
-          <input type="text" className="form-control" placeholder="Check in" />
+          <input
+            type="text"
+            onChange={(e) => console.log(e.target.value)}
+            onFocus={(e) => (e.target.type = "date")}
+            onBlur={(e) => (e.target.type = "date")}
+            className="form-control"
+            placeholder="Check in"
+          />
         </div>
+
         <div className="form-group has-search m-1">
           <span className="bi bi-calendar-week-fill form-control-feedback"></span>
-          <input type="text" className="form-control" placeholder="Check out" />
+          <input
+            ref={ref}
+            type="text"
+            onChange={(e) => console.log(e.target.value)}
+            onFocus={(e) => (e.target.type = "date")}
+            onBlur={(e) => (e.target.type = "date")}
+            className="form-control"
+            placeholder="Check out"
+          />
         </div>
+
         <Button className="m-1">Buscar</Button>
       </div>
     </>
