74b81d6f72f38568a1e88cbaa2f13a3093ee564c
Felipe Godoy <felipe-est@hotmail.com>
2022-11-08 14:14:05 -0300

    fix(app):  testing fetch data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,22 +11,25 @@ import { LoginProvider } from "./Contexts/LoginContext";
 import {ContextProvider} from "./Contexts/Context";
 import { useState, useEffect } from 'react'
 
-useEffect(() => {
-  const [ data, setData ] = useState('')
-  const fetchData = async () => {
-    try {
-      const req = await fetch('ip-172-31-9-65.us-west-1.compute.internal/')
-      setData(req)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-  fetchData()
-  console.log(data)
-}, [])
+
 
 
 export default function App() {
+
+  useEffect(() => {
+    const [ data, setData ] = useState('')
+    const fetchData = async () => {
+      try {
+        const req = await fetch('ip-172-31-9-65.us-west-1.compute.internal/')
+        setData(req)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+    console.log(data)
+  }, [])
+  
   return (
     <div className="App">
       <ContextProvider>
