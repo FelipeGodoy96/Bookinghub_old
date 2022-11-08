1cbf904b8ef0aa1847b85aa68c12ff93d8aa9685
Felipe Godoy <felipe-est@hotmail.com>
2022-11-08 18:48:56 -0300

    fix(app): testing fetch private IP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
   useEffect(() => {   
     const fetchData = async () => {
       try {
-        const request = await fetch('http://54.183.252.14:8080/imagens')
+        const request = await fetch('http://ip-172-31-9-65.us-west-1.compute.internal:8080/imagens')
         const response = await request.json()
         setData(response)
         console.log(response)
