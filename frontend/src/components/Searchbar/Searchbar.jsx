17e0e21fe35eb420b2bbad44b876565b800383f7
ViniciusFelce <97105641+ViniciusFelce@users.noreply.github.com>
2022-11-10 03:15:17 -0300

    Input buscar (API city)

--- frontend/src/components/Searchbar/Searchbar.jsx
+++ frontend/src/components/Searchbar/Searchbar.jsx
@@ -1,20 +1,55 @@
 import Button from "react-bootstrap/Button";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../../styles/Searchbar.css";
 
 export default function Searchbar() {
   const ref = useRef();
-  return (
+
+  const [Resp, Repos] = useState([])
+  const [resp, repos] = useState([])
+
+  useEffect(() => {
+    const FetchResp = async () => {
+      try{
+        const api = await fetch('http://54.183.252.14:8080/cidades')
+        const data = await api.json();
+        console.log(data);
+        Repos(data);
+        repos(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    FetchResp();
+  }, []);
+  const Change = ({ target }) => {
+    if(!target.value) {
+      repos(Resp)
+      return
+    }
+    const filterResp = resp.filter(({nome}) => nome.includes(target.value))
+    repos(filterResp);
+  }
+    return (
     <>
       <div className="searchBarComponent">
         <div className="d-flex w-100 flex-column flex-lg-row justify-content-center ">
           <div className="form-group has-search m-1 ">
             <span className="bi bi-geo-alt  form-control-feedback"></span>
+
             <input
               type="text"
+              list="data"
               className="form-control"
               placeholder="Onde Vamos?"
+              onChange={Change}
             />
+            <datalist id="data">
+              {resp.map((respost) =>
+                <option key={respost.id} value={respost.nome} />
+              )}
+            </datalist>        
+
           </div>
 
           <div className="calendar-input form-group has-search m-1">
