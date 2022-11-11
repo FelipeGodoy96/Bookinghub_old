ca318cd58a5dfbbd4ab82959832ac68a7385abd3
vitinop <victor.fernando.moura@hotmail.com>
2022-11-11 01:07:01 -0300

    deleted back end old files

--- backend/Api_Categorias/LionsRest/src/main/java/br/com/lionsrest/LionsRest/repository/RepositoryCategorias.java
+++
@@ -1,7 +0,0 @@
-package br.com.lionsrest.LionsRest.repository;
-
-import br.com.lionsrest.LionsRest.model.Categorias;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryCategorias extends JpaRepository<Categorias, Integer> {
-}
