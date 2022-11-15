bc66cb67ef38745d51399d0d4b08333036d99716
vitinop <victor.fernando.moura@hotmail.com>
2022-11-15 19:29:36 -0300

    aplicação de regras ESlint

--- frontend/src/Cadastro.jsx
+++ frontend/src/Cadastro.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable no-useless-escape */
 import React, { useRef, useState } from 'react';
 import Container from 'react-bootstrap/Container';
