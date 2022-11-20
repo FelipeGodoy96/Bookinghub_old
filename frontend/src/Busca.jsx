75ae3ee88d681499357115b6b94aa4b86ec31254
vitinop <victor.fernando.moura@hotmail.com>
2022-11-20 19:58:53 -0300

    Update Busca.jsx

--- frontend/src/Busca.jsx
+++ frontend/src/Busca.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Container from 'react-bootstrap/esm/Container';
 import Footer from './components/Footer/Footer';
 import Searchbar from './components/Searchbar/Searchbar';
