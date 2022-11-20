88674b4956bb125bfe75c3a461560d02444c17f2
vitinop <victor.fernando.moura@hotmail.com>
2022-11-20 19:55:28 -0300

    Update Busca.jsx

--- frontend/src/Busca.jsx
+++ frontend/src/Busca.jsx
@@ -3,20 +3,7 @@ import Container from 'react-bootstrap/esm/Container';
 import Footer from './components/Footer/Footer';
 import Searchbar from './components/Searchbar/Searchbar';
 
-export default function SearchResult() {
-  const [cidade, setCidade] = useState({});
-
-  useEffect(() => {
-    const FetchResp = async () => {
-      const api = await fetch('http://52.53.186.118:8080/cidade_produtos');
-      const data = await api.json();
-      // console.log(data);
-      const teste = data.find((prop) => prop.nome === 'Ouro Preto');
-      setCidade(teste);
-    };
-    FetchResp();
-  }, []);
-
+export default function Busca() {
   return (
     <div className="d-flex flex-column m-0 vh-100">
       <section className="d-flex flex-column align-items-center text-center SearchBar">
