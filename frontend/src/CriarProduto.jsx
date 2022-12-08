4098de085475f16869285924d0edbf7292570ee3
Matheus Emanoel <matheusemanoel1407@gmail.com>
2022-12-08 11:07:27 -0400

    up5

--- frontend/src/CriarProduto.jsx
+++ frontend/src/CriarProduto.jsx
@@ -14,57 +14,64 @@ export default function CriarProduto() {
 
         <div className="formContainer d-flex flex-column">
           <Container className=" d-flex align-items-center justify-content-center flex-column align-content-start flex-wrap">
-            <Card className="cardForm w-100">
+            <Card className="cardForm w-100 d-flex">
               <Card.Title>
                 <h1 className='text-center'>Criar propriedade</h1>
               </Card.Title>
 
-              <Form className='d-flex '>
+              <Form className='d-flex flex-column flex-wrap'>
                 <div className="d-flex flex-column flex-lg-row flex-md-row justify-content-between">
-                  <Row className='mb-4'>
-                    <Form.Group as={Col} className=" group">
-                      <Form.Label>Nome da propriedade</Form.Label>
-                      <Form.Control className='control' type="text" placeholder="Nome da propriedade" />
-                    </Form.Group>
 
-                    <Form.Group as={Col} className=" group">
-                      <Form.Label>Categoria</Form.Label>
-                      <Form.Select className='select' >
+                  <Form.Group className=" group">
+                    <Form.Label>Nome da propriedade</Form.Label>
+                    <Form.Control className='control' type="text" placeholder="Nome da propriedade" />
+                  </Form.Group>
 
-                        <option>Selecione uma categoria</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </Form.Select>
+                  <Form.Group className=" group">
+                    <Form.Label>Categoria</Form.Label>
+                    <Form.Select className='select' >
 
-                    </Form.Group>
-                  </Row>
+                      <option>Selecione uma categoria</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>
+
+                  </Form.Group>
                 </div>
 
                 <div className="d-flex flex-column flex-lg-row flex-md-row justify-content-between">
-                  <Row className='mb-4'>
-                    <Form.Group as={Col} className=" group">
-                      <Form.Label>Endereço</Form.Label>
-                      <Form.Control className='control' type="text" placeholder="Nome da propriedade" />
-                    </Form.Group>
 
-                    <Form.Group as={Col} className=" group">
-                      <Form.Label>Cidade</Form.Label>
-                      <Form.Select className='select' >
+                  <Form.Group className=" group">
+                    <Form.Label>Endereço</Form.Label>
+                    <Form.Control className='control' type="text" placeholder="Nome da propriedade" />
+                  </Form.Group>
 
-                        <option>Selecione uma categoria</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </Form.Select>
+                  <Form.Group className=" group">
+                    <Form.Label>Cidade</Form.Label>
+                    <Form.Select className='select' >
 
-                    </Form.Group>
-
-
-                  </Row>
-
-
+                      <option>Selecione uma categoria</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>
+                  </Form.Group>
                 </div>
+
+
+                <div className="d-flex flex-column flex-lg-row flex-md-row justify-content-between">
+
+                  <Form.Group className="group d-flex flex-column mt-2 ">
+                    <Form.Label>Descrição</Form.Label>
+                    <Form.Control
+                      as="textarea" className='textarea '
+                      placeholder="Leave a comment here"
+                     
+                    />
+                  </Form.Group>
+                </div>
+
 
 
 
