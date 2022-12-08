a965bc651406d96dd802a4a43e907e92dc3bdfa2
Felipe Godoy <felipe-est@hotmail.com>
2022-12-08 00:03:38 +0000

    Update ControllerUsuariosRegister.java

--- backend/APIrest/APIrest/src/main/java/br/com/APIrest/APIrest/controller/ControllerUsuariosRegister.java
+++ backend/APIrest/APIrest/src/main/java/br/com/APIrest/APIrest/controller/ControllerUsuariosRegister.java
@@ -1,38 +1,80 @@
 package br.com.APIrest.APIrest.controller;
 
-import br.com.APIrest.APIrest.dto.UsuariosDto;
-import br.com.APIrest.APIrest.dto.UsuariosForm;
+import br.com.APIrest.APIrest.security.EPermits;
+import br.com.APIrest.APIrest.model.Role;
 import br.com.APIrest.APIrest.model.Usuarios;
 import br.com.APIrest.APIrest.repository.RepositoryUsuarios;
-import br.com.APIrest.APIrest.security.usuario.UsuarioService;
+import br.com.APIrest.APIrest.repository.RepositoryRole;
+import br.com.APIrest.APIrest.security.InfoRequisicao;
+import br.com.APIrest.APIrest.security.UsuariosRegister;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
+import java.util.HashSet;
+import java.util.Set;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping(value = "/usuarios")
-public class ControllerUsuarios {
+@RequestMapping("/api/auth")
+public class ControllerUsuariosRegister {
 
     @Autowired
-    RepositoryUsuarios usuariosRepository;
+    RepositoryUsuarios repositoryUsuarios;
     @Autowired
-    UsuarioService usuarioService;
+    RepositoryRole repositoryRole;
+    @Autowired
+    PasswordEncoder encoder;
 
-    @PostMapping
-    @Transactional
-    public ResponseEntity<UsuariosDto> cadastra(@RequestBody UsuariosForm usuarioForm){
+        @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UsuariosRegister signUpRequest) {
+        if (repositoryUsuarios.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new InfoRequisicao("Erro: O Email já está Registrado!"));
+        }
 
-        Usuarios novoUsuario = usuarioForm.toEntity();
-        usuariosRepository.save(novoUsuario);
+        Usuarios usuarios = new Usuarios(signUpRequest.getUsername(),
+                signUpRequest.getNome(),
+                signUpRequest.getSobrenome(),
+                encoder.encode(signUpRequest.getPassword()));
 
-        return ResponseEntity.ok(novoUsuario.toDto());
-    }
-    @Transactional
-    @GetMapping (value = "/{id}")
-    public ResponseEntity<UsuariosDto> findUsuariosById(@PathVariable Integer id) {
-        UsuariosDto dto = usuarioService.findById(id);
-        return ResponseEntity.ok().body(dto);
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = repositoryRole.findByName(EPermits.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Erro: a Função" +
+                            "" +
+                            " não encontrado."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = repositoryRole.findByName(EPermits.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Erro: Função não encontrada."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = repositoryRole.findByName(EPermits.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Erro: Função não encontrada."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = repositoryRole.findByName(EPermits.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Erro: Função não encontrada."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        usuarios.setRoles(roles);
+        repositoryUsuarios.save(usuarios);
+
+        return ResponseEntity.ok(new InfoRequisicao("Usuario Registrado com sucesso."));
     }
 }
