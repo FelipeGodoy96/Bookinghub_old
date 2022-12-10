868df406ca89a082ba38afde85f506440e55683f
Matheus Emanoel <matheusemanoel1407@gmail.com>
2022-12-10 10:35:30 -0400

    up1

--- frontend/src/MinhasReservas.jsx
+++ frontend/src/MinhasReservas.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer/Footer';
+import Noproduct from './components/LottieAnimations/Noproduct';
 import MinhasReservasCard from './components/MinhasReservasCard/MinhasReservasCard';
 
 export default function MinhasReservas() {
   return (
     <>
       <div className="d-flex flex-column">
-        <Container className="d-flex flex-column mt-5 mb-5 ">
+        <Container className="d-flex flex-column mb-5 mt-5   ">
+          <Noproduct/>
           <MinhasReservasCard />
         </Container>
       </div>
---
+++ frontend/src/components/LottieAnimations/Noproduct.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import Button from 'react-bootstrap/esm/Button';
+import { Link, useNavigate } from 'react-router-dom';
+import NoproductJson from '../../assets/lottie/no-product.json';
+
+export default function Noproduct() {
+  const navigate = useNavigate();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: NoproductJson,
+  };
+
+  return (
+    <div className="d-flex row text-center justify-content-center align-content-center mt-5">
+     
+      <Lottie options={defaultOptions} width={250} height={250} />
+      <h2>Você ainda não possui nenhuma reserva.</h2>
+      <Button className="m-2 no-product-btn" onClick={() => navigate('/')}>
+            Fazer Reserva
+          </Button>
+
+    </div>
+  );
+}
