58293aed0a5a70a99f087665ce9635649d6c4dfd
Felipe Godoy <felipe-est@hotmail.com>
2022-12-11 18:51:10 -0300

    feat(addProduct-modal): form logic implemented, creating product is working, minor things to implement yet

--- frontend/src/components/AdminPanelModals/AddProduct.jsx
+++ frontend/src/components/AdminPanelModals/AddProduct.jsx
@@ -17,7 +17,45 @@ export default function AddProduct (props) {
   const [ title, setTitle ] = useState("")
   const { state } = React.useContext(Context)
   const [ categories, setCategories ] = useState([])
+  const [ name, setName ] = useState("")
+  const [ category, setCategory ] = useState("")
+  const [ city, setCity ] = useState("")
+  const [ description, setDescription ] = useState("")
+  const [ icon, setIcon ] = useState("")
+  const [ iconName, setIconName ] =  useState("")
+  const [ newCity, setNewCity ] = useState("")
+
+  const apiLink = process.env.NODE_ENV === 'development' ? 'http://13.57.207.104:8080' : '';
   
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const newCityData = {
+      nome: city,
+      pais: ''
+    }
+    const newProductData = {
+      nome: name,
+      categoria_onetm: category,
+      cidade_mtone: newCity,
+      descricao: description,
+    }
+
+    try {
+      await axios.post(`${apiLink}/cidades`, newCityData)
+      .then((res) => {
+        setNewCity(res.data.id)
+      })
+    } catch (error) {
+      console.error(error)
+    }
+    try {
+      await axios.post(`${apiLink}/produtos`, newProductData)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
   
   
 
@@ -34,24 +72,29 @@ export default function AddProduct (props) {
               <Card.Title>
                 <span className="cardTitle"></span>
               </Card.Title>
-              <Form className='d-flex flex-column flex-wrap p-3' onSubmit={() => {
-                {'lógica de envio do formulario aqui'}
-              }}>
+              <Form className='d-flex flex-column flex-wrap p-3'>
                 <div className="d-flex flex-column flex-lg-row flex-md-row justify-content-between">
 
                   <Form.Group className=" group m-3 ">
-                    <Form.Label>Nome da propriedade</Form.Label>
-                    <Form.Control className='control' type="text" placeholder="Nome da propriedade" />
+                    <Form.Label>Nome da propiedade</Form.Label>
+                    <Form.Control className='control' type="text" placeholder="" onChange={(e) => {
+                      setName(e.target.value)
+                    }}/>
                   </Form.Group>
 
                   <Form.Group className=" group m-3  ">
                     <Form.Label>Categoria</Form.Label>
-                    <Form.Select className='select' >
+                    <Form.Select className='select' onChange={(e) => {
+                      
+                      setCategory(e.target.selectedOptions[0].value)
+                    }}>
 
                       <option>Selecione uma categoria</option>
-                      <option value="1">One</option>
-                      <option value="2">Two</option>
-                      <option value="3">Three</option>
+                      <option value="1">Hotel</option>
+                      <option value="2">Hotel Fazenda</option>
+                      <option value="3">Apartamento</option>
+                      <option value="4">Hostel</option>
+                      
                     </Form.Select>
 
                   </Form.Group>
@@ -59,20 +102,13 @@ export default function AddProduct (props) {
 
                 <div className="d-flex flex-column flex-lg-row flex-md-row justify-content-between">
 
-                  <Form.Group className=" group m-3">
-                    <Form.Label>Endereço</Form.Label>
-                    <Form.Control className='control' type="text" placeholder="Endereço" />
-                  </Form.Group>
+                  
 
                   <Form.Group className=" group m-3">
                     <Form.Label>Cidade</Form.Label>
-                    <Form.Select className='select' >
-
-                      <option>Selecione uma Cidade</option>
-                      <option value="1">One</option>
-                      <option value="2">Two</option>
-                      <option value="3">Three</option>
-                    </Form.Select>
+                    <Form.Control className='control' type="text" placeholder="" onChange={(e) => {
+                      setCity(e.target.value)
+                    }}/>
                   </Form.Group>
                 </div>
 
@@ -83,44 +119,33 @@ export default function AddProduct (props) {
                     <Form.Label>Descrição</Form.Label>
                     <Form.Control
                       as="textarea" className='textarea '
-                      placeholder="Leave a comment here"
+                      placeholder=""
+                      onChange={(e) => {setDescription(e.target.value)}}
 
                     />
                   </Form.Group>
                 </div>
-
-             
-
-            
-          
-            
                   <Card.Title>
                     <span className="cardTitle">Adicionar atributos</span>
                   </Card.Title>
                  
                     <Form.Group className=" group m-3 ">
-                      <Form.Label>Nome</Form.Label>
-                      <Form.Control className='control' type="text" placeholder="Nome do atributo" />
+                      <Form.Label>Nome do atributo</Form.Label>
+                      <Form.Control className='control' type="text" placeholder="" />
                     </Form.Group>
 
                    
                     <Form.Group className=" group m-3 ">
                       <Form.Label>Icone</Form.Label>
                       <div className='d-flex flex-row align-items-center'>
-                      <Form.Control className='control ' type="text" placeholder="Icone" />
+                      <Form.Control className='control ' type="text" placeholder="Ex.: fa fa-wifi" />
                       <button className="plus-button mx-3"></button>
                        </div>
                     </Form.Group>
-                   
-               
-                 
-               
-
-               
-                <Card.Title>
+                {/* <Card.Title>
                     <span className="cardTitle">Políticas do produto</span>
-                  </Card.Title>
-                  <div className='d-flex flex-wrap justify-content-center '>
+                  </Card.Title> */}
+                  {/* <div className='d-flex flex-wrap justify-content-center '>
                   <div className='regras mt-5'>
                      <h4>Regras da casa</h4> 
                     
@@ -158,8 +183,10 @@ export default function AddProduct (props) {
                   </Form.Group>
                  
                   </div>
-                  </div><button className="createProduct">Criar</button>
+                  </div>*/}
+                  <button type="button" onClick={handleSubmit} className="createProduct">Criar</button>
                   </Form>
+                  
                 </Card>
       </Modal.Body>
     </Modal>
