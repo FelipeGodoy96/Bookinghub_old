5bdea2ac235b938461f0dbc293a5a7182c105a81
Felipe Godoy <felipe-est@hotmail.com>
2022-12-15 17:24:05 -0300

    fix(Admin-Panel): product creation

--- frontend/src/components/Modal/ModalProduto.jsx
+++ frontend/src/components/Modal/ModalProduto.jsx
@@ -18,14 +18,14 @@ export default function ModalProduto({ visible }) {
   return (
     <Modal className="modal" show={show} onHide={() => setShow(false)}>
       <Modal.Header closeButton>
-        <Modal.Title>Muito Obrigado!</Modal.Title>
+        <Modal.Title>Operação concluída</Modal.Title>
       </Modal.Header>
       <Modal.Body className="d-flex flex-column align-items-center justify-content-center p-4">
         <BsFillPatchCheckFill className="checkfill" />
         Produto criado com sucesso!
       </Modal.Body>
       <Modal.Footer className="d-flex justify-content-center">
-        <Button className="btn-1" id="NavegarButtonModal" variant="secondary" onClick={() => handleRedirect('/')}>
+        <Button className="btn-1" id="NavegarButtonModal" variant="secondary" onClick={() => handleRedirect('/admin')}>
           Continuar Navegando
         </Button>
       </Modal.Footer>
