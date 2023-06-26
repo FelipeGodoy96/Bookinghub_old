e6b796721b2f53ef484c0c2d3f1c8b80673c524f
Felipe Godoy <felipe-est@hotmail.com>
2023-06-26 01:04:54 +0100

    few improvements to product card styling

--- frontend-rework/src/components/ProductCard/index.jsx
+++ frontend-rework/src/components/ProductCard/index.jsx
@@ -1,9 +1,20 @@
-import './styles.css'
+import "./styles.css";
 
-export const ProductCard = ({product}) => {
-    return (
-        <div className='product flex flex-col bg-white rounded-b-lg overflow-hidden drop-shadow-md shadow-lg'>
-            <img src={product.source} className='w-full rounded-b-md' />
+export const ProductCard = ({ product }) => {
+  return (
+    <div className="product-card flex flex-col sm:flex-row bg-white rounded-b-lg overflow-hidden drop-shadow-md shadow-lg w-full">
+      <img src={product.source} className="w-full sm:w-1/3 rounded-b-md" />
+      <div className="card-description flex flex-col">
+        <div className="title">
+          <article>
+            <h3 className="text-slate-500">
+              {product.tipo.toUpperCase()} <span>{product.estrelas}</span>
+            </h3>
+            <h1 className="font-bold">{product.nome}</h1>
+          </article>
         </div>
-    )
-}
+        <div className="text"></div>
+      </div>
+    </div>
+  );
+};
