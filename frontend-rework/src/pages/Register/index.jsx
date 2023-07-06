da95e0bb9da6060cc5a738f1b5ca01ae6afecc0c
Felipe Godoy <felipe-est@hotmail.com>
2023-07-06 01:12:14 +0100

    finishing stylization of register page mobile version

--- frontend-rework/src/pages/Register/index.jsx
+++ frontend-rework/src/pages/Register/index.jsx
@@ -5,36 +5,35 @@ import { Link } from 'react-router-dom'
 export const Register = () => {
     return (
         <section className='register absolute w-full flex justify-center items-center align-center overflow-hidden'>
-            <div className='register_content'>
-            <span className='content_text font-bold my-6'>Criar conta</span>
-                <form className='register-form flex flex-col justify-between items-center w-full gap-6' onSubmit={(event) => {
+            <div className='register_content text-center'>
+            <span className='login_content-text font-bold my-6'>Criar conta</span>
+                <form className='register_content-form flex flex-col justify-between items-center w-full gap-4 text-base' onSubmit={(event) => {
                     event.preventDefault()
                 }}>
-                    <div className='register-email w-full text-left flex flex-col gap-2'>
+                    <div className='register-email w-full text-left flex flex-col gap-1'>
                         <label htmlFor='email' className='px-1'>Nome</label>
-                        <input type="email" name="email" className='w-full rounded-md h-10 appearance-none py-3 px-2'/>
+                        <input type="email" name="email" className='w-full rounded-md h-8 appearance-none py-3 px-2'/>
                     </div>
-                    <div className='register-password w-full text-left flex flex-col gap-2'>
+                    <div className='register-password w-full text-left flex flex-col gap-1'>
                         <label htmlFor='password' className='px-1'>Sobrenome</label>
-                        <input type="password" name="password" className='w-full rounded-md h-10 appearance-none py-3 px-2'/>
+                        <input type="password" name="password" className='w-full rounded-md h-8 appearance-none py-3 px-2'/>
                     </div>
-                    <div className='register-password w-full text-left flex flex-col gap-2'>
+                    <div className='register-password w-full text-left flex flex-col gap-1'>
                         <label htmlFor='password' className='px-1'>E-mail</label>
-                        <input type="password" name="password" className='w-full rounded-md h-10 appearance-none py-3 px-2'/>
+                        <input type="password" name="password" className='w-full rounded-md h-8 appearance-none py-3 px-2'/>
                     </div>
-                    <div className='register-password w-full text-left flex flex-col gap-2'>
+                    <div className='register-password w-full text-left flex flex-col gap-1'>
                         <label htmlFor='password' className='px-1'>Senha</label>
-                        <input type="password" name="password" className='w-full rounded-md h-10 appearance-none py-3 px-2'/>
+                        <input type="password" name="password" className='w-full rounded-md h-8 appearance-none py-3 px-2'/>
                     </div>
-                    <div className='register-password w-full text-left flex flex-col gap-2'>
+                    <div className='register-password w-full text-left flex flex-col gap-1'>
                         <label htmlFor='password' className='px-1'>Confirmar senha</label>
-                        <input type="password" name="password" className='w-full rounded-md h-10 appearance-none py-3 px-2'/>
+                        <input type="password" name="password" className='w-full rounded-md h-8 appearance-none py-3 px-2'/>
                     </div>
                     <div className='register-button mt-8'>
-                        <Button text='Entrar' variant='primary' width='12.875rem' type='submit'/>
+                    <Button text='Criar conta' variant='primary' width='19.375rem' fontSize="16px" type='submit'/>
                     </div>
-                    <span className='text-sm'>Ainda não tem conta? <Link to='/register' className='text-cyan-500'>Registre-se.</Link></span>
-                    <span className='text-sm'>Esqueceu sua senha? <Link to='/forgotpw' className='text-cyan-500'>Recupere-a.</Link></span>
+                    <span className='text-sm'>Já tem uma conta? <Link to='/login' className='text-cyan-500'>Iniciar sessão</Link></span>
                 </form>
             </div>
         </section>
