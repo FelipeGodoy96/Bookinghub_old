0fc4125c317e44e94a93302cb2deca087a02f8ba
Felipe Godoy <felipe-est@hotmail.com>
2023-07-06 17:59:34 +0100

    finishing search suggestions on searchbar component, must fix the positioning of dropdown

--- frontend-rework/src/components/Searchbar/index.jsx
+++ frontend-rework/src/components/Searchbar/index.jsx
@@ -3,18 +3,63 @@ import { Button } from "../Button";
 import { useState } from "react";
 import { useEffect } from "react";
 
-
 export const Searchbar = () => {
-  const [searchTerm, setSearchTerm] = useState('')
-  const [suggestions, setSuggestions] = useState([])
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false);
 
+  // This will be used when the implementation on backend is finished
+  // useEffect(() => {
+  //   fetch("/api/frequentsearchterms")
+  //     .then((response) => response.json())
+  //     .then((data) => setSuggestions(data));
+  // }, []);
+
+  const frequentSearchTerms = [
+    {
+      destino: "San Carlos de Bariloche",
+      pais: "Argentina",
+    },
+    {
+      destino: "Buenos Aires",
+      pais: "Argentina",
+    },
+    {
+      destino: "Mendoza",
+      pais: "Argentina",
+    },
+    {
+      destino: "Córdoba",
+      pais: "Argentina",
+    }
+  ];
+
+
+  // Used to test 
   useEffect(() => {
-    fetch('/api/frequentsearchterms')
-    .then(response => response.json())
-    .then(data => setSuggestions(data))
-  }, [])
+    setSuggestions(frequentSearchTerms);
+  }, []);
 
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+    // Filtering the frequent search terms based on the user's input
+    const filteredSuggestions = frequentSearchTerms.filter((term) => term.destino.toLowerCase().includes(value.toLowerCase()))
+    setSuggestions(filteredSuggestions)
+  };
 
+  const handleInputFocus = () => {
+    setShowDropdown(true);
+  };
+
+  const handleInputBlur = () => {
+    setShowDropdown(false);
+  };
+
+  const handleOptionClick = (option) => {
+    setSearchTerm(option.target.textContent);
+    setShowDropdown(false);
+  };
 
   return (
     <div className="searchbar flex flex-col justify-center gap-2 text-center px-2 text-white font-bold h-auto">
@@ -23,11 +68,74 @@ export const Searchbar = () => {
       </div>
       <div className="searchbar-search flex flex-col justify-center md:flex-row gap-2 py-4 lg:mx-32 md:mx-6 relative">
         <input
-          onFocus={{}}
+          type="text"
+          value={searchTerm}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          // onBlur={handleInputBlur}
           placeholder="Onde vamos?"
-          className="search-location rounded h-10 pl-10 text-sm font-semibold lg:w-1/3"
+          className="search-location rounded h-10 pl-10 text-sm font-semibold lg:w-1/3 text-slate-500"
         />
-        
+        {showDropdown && (
+          <div className="dropdown relative overflow-hidden z-10 bg-white rounded-b-lg shadow-lg w-full">
+            <ul className="mx-2 text-center">
+              {suggestions.map((option, index) => (
+                <>
+                  <li
+                    className="text-black flex justify-start gap-4 items-center my-4"
+                    key={index}
+                    onClick={handleOptionClick}
+                  >
+                    <svg
+                      key={index}
+                      className="icon-svg w-6"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M7.37584 4.85915C8.65549 3.61971 10.1969 3 12 3C13.8031 3 15.3372 3.61267 16.6023 4.83803C17.8675 6.06339 18.5 7.54929 18.5 9.29577C18.5 10.169 18.2746 11.169 17.8238 12.2958C17.373 13.4225 16.8277 14.4789 16.1879 15.4648C15.5481 16.4507 14.9156 17.3732 14.2903 18.2324C13.665 19.0916 13.1342 19.7746 12.698 20.2817L12 21C11.8255 20.8028 11.5928 20.5423 11.302 20.2183C11.0112 19.8944 10.4877 19.2465 9.73154 18.2746C8.97539 17.3028 8.31376 16.3592 7.74664 15.4437C7.17953 14.5282 6.66331 13.493 6.19799 12.338C5.73266 11.1831 5.5 10.169 5.5 9.29577C5.5 7.54929 6.12527 6.07043 7.37584 4.85915Z"
+                          stroke="#545776"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        ></path>{" "}
+                        <path
+                          d="M13 9.5C13 10.0523 12.5523 10.5 12 10.5C11.4477 10.5 11 10.0523 11 9.5C11 8.94772 11.4477 8.5 12 8.5C12.5523 8.5 13 8.94772 13 9.5Z"
+                          stroke="#545776"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        ></path>{" "}
+                      </g>
+                    </svg>
+                    <p className="text-slate-700">
+                      {option.destino}
+                    </p>
+                    
+                  </li>
+                  <hr
+                    className={
+                      index + 1 == suggestions.length
+                        ? "hidden"
+                        : "" + "mx-1 bg-cyan-500 h-0.5 "
+                    }
+                  />
+                </>
+              ))}
+            </ul>
+          </div>
+        )}
+
         <div className="location-icon absolute left-2 top-6">
           <svg
             className="icon-svg w-6"
