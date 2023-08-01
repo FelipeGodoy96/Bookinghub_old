cdcfb39c11fd5de6f2091fad93539b95b1222766
Felipe Godoy <felipe-est@hotmail.com>
2023-08-01 18:12:30 +0100

    changed allowed origins

--- backend-rework/src/main/java/br/com/bookinghubgodoynetworks/api/controller/CategoryController.java
+++ backend-rework/src/main/java/br/com/bookinghubgodoynetworks/api/controller/CategoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RequestMapping("/categories")
 public class CategoryController {
 
