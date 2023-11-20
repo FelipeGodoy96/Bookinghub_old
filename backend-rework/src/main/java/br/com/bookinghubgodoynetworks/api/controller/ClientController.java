68e782d2d61e6a16039dda0c5685e8ffd4cd41c2
Felipe Godoy <99193801+FelipeGodoy96@users.noreply.github.com>
2023-11-20 16:01:44 -0300

    Update ClientController.java

--- backend-rework/src/main/java/br/com/bookinghubgodoynetworks/api/controller/ClientController.java
+++ backend-rework/src/main/java/br/com/bookinghubgodoynetworks/api/controller/ClientController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "**")
 @RequestMapping("/clients")
 public class ClientController {
 
